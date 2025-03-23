3a0457b64f7058a7920917530d7f6fc4dea0d470
Mihailo Markovic <markovic.mihailo98@gmail.com>
2025-03-23 17:54:15 +0100

    Create theme change functions

--- script.js
+++ script.js
@@ -4,7 +4,30 @@ const body = document.querySelector("body");
 const btnSlider = document.querySelector(".theme__slider");
 let currTheme = localStorage.getItem("theme");
 
-if (currTheme !== 1) {
+function setDefaultTheme() {
   localStorage.setItem("theme", "1");
   currTheme = 1;
 }
+
+if (currTheme === null) {
+  setDefaultTheme();
+} else {
+  body.classList.add(`theme_${currTheme}`);
+}
+
+const changeTheme = function () {
+  if (currTheme < 3) {
+    body.classList.remove(`theme_${currTheme}`);
+    ++currTheme;
+    body.classList.add(`theme_${currTheme}`);
+  } else if (currTheme == 3) {
+    body.classList.remove(`theme_${currTheme}`);
+    setDefaultTheme();
+    body.classList.add(`theme_${currTheme}`);
+  }
+  console.log(currTheme);
+
+  localStorage.setItem("theme", currTheme);
+};
+
+btnSlider.addEventListener("click", changeTheme);
